8aae260cbbd4433482480b4f08346f81b340e305
Herbert M. Turner <herbieturner@verizon.net>
2018-05-24 13:14:03 -0400

    Updated scraper, created regex expressions

--- test_scrape.py
+++ test_scrape.py
@@ -5,13 +5,14 @@ from bs4 import BeautifulSoup
 
 def collect_cases():
     url = r'https://www.courtlistener.com/api/rest/v3/opinions'
-    header = 'Token dffce215ac12d401f8915695a2dfee2fc399c8f6'
-    response = requests.get(url, header)
+    head={'Authorization': 'Token dffce215ac12d401f8915695a2dfee2fc399c8f6'}
+    response = requests.get(url, headers = head)
     opinions_json = response.json()
     text_documents = []
-    #print(opinions_json) -Uncomment if you need to check api cooldown time
-    for x in range(1):
-    # while opinions_json["next"] is not None:
+    #print(opinions_json) Uncomment to check if throttled
+    last_link = ""
+    for x in range(200):
+        print(x)
         for x in range(len(opinions_json["results"])):
             if opinions_json["results"][x]["html_lawbox"]:
                 print(opinions_json["results"][x]["id"])
@@ -21,21 +22,37 @@ def collect_cases():
                 print(opinions_json["results"][x]["id"])
                 html = opinions_json["results"][x]["plain_text"]
                 text_documents.append([html,opinions_json["results"][x]["id"]])
-        response = requests.get(opinions_json["next"])
+            elif opinions_json["results"][x]["html"]:
+                print(opinions_json["results"][x]["id"])
+                html = opinions_json["results"][x]["html"]
+                text_documents.append([html,opinions_json["results"][x]["id"]])
+            else:
+                print("Insufficient Data(I hate this API).")
+        last_link = opinions_json["next"]
+        response = requests.get(opinions_json["next"],headers = head)
         opinions_json = response.json()
-    # print(opinions_json)
-    # for case in opinions_json:
-    #     print(case)
-    # with open("dat.txt", w) as outfile:
-    #     json.dump(data, outfile)
-
-    print(text_documents)
-    f = open("raw_cases.txt","w+")
+    d = open("last_link.txt","a")
+    d.write(last_link)
+    d.close()
     for x in text_documents:
-        f.write(str(x))
+        f = open("raw_cases.txt","a")
+        try:
+            f.write(str(x[1]))
+            f.write("\n")
+            print(x[1])
+            print("Written")
+            f.close()
+        except UnicodeEncodeError:
+            print("Can't write!")
+        f.close()
     return text_documents
+
+states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota",
+"Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia",
+"Wisconsin","Wyoming"]
+
 def parse_court_case(entry):
-    data = [] #id,civil rights case, year, court
+    data = [] #id,civil rights case, year, state
     data.append(entry[1])
     #Check civil rights
     civil_rights = False
@@ -44,15 +61,22 @@ def parse_court_case(entry):
         if word in entry[0]:
             if len(data) < 2:
                 data.append(True)
+    if len(data) < 2:
+        data.append(False)
     #Check year
-    start = entry[0].index("(")
-    end = entry[0].index(")")
-    data.append(entry[0][start+1:end])
+    if "(" in entry[0][:]:
+        start = entry[0].index("(")
+        end = entry[0].index(")")
+        data.append(entry[0][start+1:end])
+    else:
+        #Work on this
+        data.append("Date Unknown")
     #Check court
-    if entry[0].index("United States District Court, "):
-        start = entry[0].index("United States District Court, ") + len("United States District Court, ")
-        entry[0].index(" ",[start:])
-    else :
+    for x in states:
+        if x in entry[0][:600]:
+            data.append(x)
+            break
+    if len(data) < 4:
         data.append("Invalid entry")
     #Write data
     return data
@@ -61,8 +85,15 @@ def parse_court_case(entry):
 raw_cases = collect_cases()
 clean_cases = []
 for x in raw_cases:
-    clean_cases.append(parse_court_case(x))
-
-f = open("clean_cases.txt","w+")
-for x in clean_cases:
-    f.write(str(x))
+    clean_case = parse_court_case(x)
+    clean_cases.append(clean_case)
+    f = open("clean_cases.txt","a")
+    f.write(str(clean_case))
+    f.write("\n")
+    f.close()
+###Regex Expressions
+#Dates
+#(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)\s+\d{1,2},\s+\d{4}
+#[(]\d{4}[)]
+#States
+#([A-Z][.]){2}[ ]|([A-Z]{2}[ ])
